8a41e489061b90d4c27c0cf0f191f176f1773d82
Nam Nguyen <buckynguyen0901@gmail.com>
2025-01-24 12:15:57 -0500

    fix reset pass

--- src/app/api/auth/confirm/route.ts
+++ src/app/api/auth/confirm/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
-  const next = searchParams.get('next') ?? '/';
+  const redirectUrl = searchParams.get('redirectUrl') ?? '/';
 
   if (token_hash && type) {
     const supabase = await createSupabaseClient();
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
     });
     if (!error) {
       // redirect user to specified redirect URL or root of app
-      redirect(next);
+      redirect(redirectUrl);
     }
   }
 
